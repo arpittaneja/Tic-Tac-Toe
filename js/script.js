3b6b3107f619e7cb281960bc28e870c10432940c
Arpit Taneja <88012@arsd.du.ac.in>
2022-08-09 00:42:39 +0530

    Add player name integration

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const Player = (name, turn) => {
     const getName = () => name;
     const getTurn = () => turn;
-
     const marker = (turn === 0) ? "X" : "O";
     const getMarker = () => marker;
 
@@ -11,7 +10,7 @@ const Player = (name, turn) => {
 const gameBoardModule = (() => {
     const gameBoard = document.querySelector(".game-board");
     const squareArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    const fillSquares = () => {
+    const newGame = () => {
         gameBoard.textContent = "";
         for (let i = 0; i < 9; i++) {
             const square = document.createElement("div");
@@ -19,61 +18,48 @@ const gameBoardModule = (() => {
             gameBoard.appendChild(square)
             square.dataset.number = squareArray[i];
         }
-    }
 
-    // const square = document.createElement("div");
-    // square.classList.add("square");
-    // gameBoard.appendChild(square);
-
-    // const square2 = document.createElement("div");
-    // square2.classList.add("square");
-    // gameBoard.appendChild(square2);
-
-    const getSquares = () => document.querySelectorAll(".square");
-    // const squares = 
-    // console.log(squares);
-
-    const fun = () => {
-        const squares = document.querySelectorAll(".square");
+        squares = getSquares();
         for (let square of squares) {
             square.addEventListener("click", click);
-            // console.log(square);
         }
-    };
+    }
+
+    const getSquares = () => document.querySelectorAll(".square");
+
+    // const addClick = () => {
+    //     const squares = document.querySelectorAll(".square");
+    //     for (let square of squares) {
+    //         square.addEventListener("click", click);
+    //     }
+    // };
 
 
     function click(e) {
         if (e.target.dataset.status !== "occupied") {
-            // if (count % 2 === 0) {
-            // console.log(e.target);
             e.target.textContent = playerArray
             [playerPointer].getMarker();
             e.target.dataset.marker = playerArray
             [playerPointer].getMarker();
-            console.log(e.target.dataset.marker);
             playerPointer = (playerPointer === 0) ? 1 : 0;
-            // }
-            // else {
-            // e.target.textContent = "O";
-            // }
             e.target.dataset.status = "occupied";
-            // count++;
         }
-        checkWinner();
+        findWinPattern();
     }
 
-    const checkWinner = () => {
+    const findWinPattern = () => {
         let squares = getSquares();
         console.log(squares);
-        console.log(squares[0].dataset.marker)
-        console.log(squares[1].dataset.marker)
-        console.log(squares[2].dataset.marker)
 
         let i = 0;
         while (i < 9) {
-            if (squares[i].dataset.marker === squares[i+1].dataset.marker && squares[i+1].dataset.marker === squares[i+2].dataset.marker && squares[i].dataset.marker !== undefined) {
-                alert("You won");
-                fillSquares();
+            if (squares[i].dataset.marker === squares[i + 1].dataset.marker && squares[i + 1].dataset.marker === squares[i + 2].dataset.marker && squares[i].dataset.marker !== undefined) {
+                for (let player of playerArray) {
+                    if (player.getMarker() === squares[i].dataset.marker) {
+                        displayWinnerName(player.getName());
+                    }
+                }
+                newGame();
                 return;
             }
             i = i + 3;
@@ -82,8 +68,12 @@ const gameBoardModule = (() => {
         let j = 0;
         while (j < 3) {
             if (squares[j].dataset.marker === squares[j + 3].dataset.marker && squares[j + 3].dataset.marker === squares[j + 6].dataset.marker && squares[j].dataset.marker !== undefined) {
-                alert("You won");
-                fillSquares();
+                for (let player of playerArray) {
+                    if (player.getMarker() === squares[j].dataset.marker) {
+                        displayWinnerName(player.getName());
+                    }
+                };
+                newGame();
                 return;
             }
             j++;
@@ -93,60 +83,57 @@ const gameBoardModule = (() => {
         while (k < 3) {
             if (k === 0) {
                 if (squares[k].dataset.marker === squares[k + 4].dataset.marker && squares[k + 4].dataset.marker === squares[k + 8].dataset.marker && squares[k].dataset.marker !== undefined) {
-                    alert("You won");
-                    fillSquares();
+                    for (let player of playerArray) {
+                        if (player.getMarker() === squares[k].dataset.marker) {
+
+                            displayWinnerName(player.getName());
+                        }
+                    }
+                    newGame();
                     return;
                 }
-                
             }
 
             else {
                 if (squares[k].dataset.marker === squares[k + 2].dataset.marker && squares[k + 2].dataset.marker === squares[k + 4].dataset.marker && squares[k].dataset.marker !== undefined) {
-                    alert("You won");
-                    fillSquares();
+                    for (let player of playerArray) {
+                        if (player.getMarker() === squares[k].dataset.marker) {
+
+                            displayWinnerName(player.getName());
+                        }
+                    }
+                    newGame();
                     return;
                 }
             }
-            k+=2;
+            k += 2;
         }
     }
 
+    const displayWinnerName = (name) => {
+        alert(name + " won");
+        playerPointer = 0;
+    }
 
-    // if (squares[0].dataset.marker === squares[1].dataset.marker && squares[1].dataset.marker === squares[2].dataset.marker && squares[0].dataset.marker !== undefined) {
-    //     alert("You won");
-    // }
-    // let {player} = 
-    // const list = (player) => {
-    //     const squares = document.querySelectorAll(".square");
-    //     for (let square of squares) {
-    //         square.addEventListener("click", (player) => {
-
-    //         })
-    //     }
-    // }
-    return { fun, fillSquares, click, getSquares, checkWinner };
+    return { newGame, getSquares, checkWinner: findWinPattern };
 })();
 
 
 // const playerOneName = prompt("Enter Player 1 name", "Player 1");
 // const playerTwoName = prompt("Enter Player 2 name", "Player 2");
+const playerOne = Player("One", 0);
+const playerTwo = Player("Two", 1);
 
-gameBoardModule.fillSquares();
-gameBoardModule.fun();
-
-// console.log(gameBoardModule.getSquares());
-// gameBoardModule.checkWinner();
-
-const playerOne = Player("playerOneName", 0);
-console.log(playerOne.getMarker("playerOneName"));
-
-const playerTwo = Player("playerTwoName", 1);
-console.log(playerTwo.getMarker("playerTwoName"));
 let playerPointer = 0;
-playerArray = [playerOne, playerTwo];
+const playerArray = [playerOne, playerTwo];
+gameBoardModule.newGame();
+// gameBoardModule.fun();
 
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", restartGame);
 
-// console.log(playerArray);
-// console.log(gameBoardModule.squares);
-
-//addEventListener to all boxes
+function restartGame(e){
+    gameBoardModule.newGame();
+}
+// console.log(playerOne.getMarker("playerOneName"));
+// console.log(playerTwo.getMarker("playerTwoName"));
