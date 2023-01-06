dbaf95dd5c8cc38bdf6a4106ddb26138815dd13c
Arpit Taneja <88012@arsd.du.ac.in>
2023-01-06 18:56:53 +0530

    Implement minimax algo for unbeatable AI option

--- js/script.js
+++ js/script.js
@@ -13,13 +13,10 @@ const gameBoard = (() => {
     let currentBoardState = null;
     let squaresArray = null;
     let unoccupiedSquaresIndexes = null;
+    let winnerInfo = null;
 
     function getUnoccupiedSquareIndexes(currentBoardState) {
         return currentBoardState.filter(i => i != "X" && i != "O");
-    }
-
-    const updateCurrentBoardStatus = (index, marker) => {
-        currentBoardState.splice(index, 1, marker);
     }
 
     const newGame = () => {
@@ -31,54 +28,51 @@ const gameBoard = (() => {
         for (let i = 0; i < 9; i++) {
             const square = document.createElement("div");
             square.classList.add("square");
-            // if (i === 1 || i === 4 || i === 2 || i === 8) {
-            square.dataset.status = "unoccupied";
             square.textContent = "";
-            // }
-
-            square.addEventListener("click", onClick);
+            square.addEventListener("click", onPlayerClick);
             gameBoardContainer.appendChild(square)
         }
-        // unoccupiedSquares = getSquares();
-        squaresArray = getSquares();
-        // unoccupiedSquaresIndexes = [1, 2, 4, 8];
-        // currentBoardState = ["X", 1, 2, "X", 4, "X", "O", "O", 8];
-        // for (let i = 0; i < 9; i++) {
-        //     if (typeof currentBoardState[i] === "string") {
-        //         squaresArray[i].textContent = currentBoardState[i];
-        //     }
-        // }
 
-        currentBoardState = [1, 2, 3, 4, 5, 6, 7, 8]
-        unoccupiedSquaresIndexes = [1, 2, 3, 4, 5, 6, 7, 8]
+        squaresArray = getSquares();
+        currentBoardState = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        unoccupiedSquaresIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     }
 
-    const onClick = (e) => {
-        if (e.target.dataset.status !== "occupied") {
-            e.target.textContent = displayController.playerArray
-            [displayController.playerPointer].getMarker();
+    const onPlayerClick = (e) => {
+        if (e.target.textContent === "") {
+            const playerMarker = displayController.playerArray[displayController.playerPointer].getMarker();
 
-            e.target.dataset.status = "occupied";
+            //update the textContent of the clicked square to the current player's marker
+            e.target.textContent = playerMarker;
 
-            updateCurrentBoardStatus(squaresArray.indexOf(e.target), displayController.playerArray[displayController.playerPointer].getMarker());
+            //update the currentBoardState array to reflect the new marker
+            currentBoardState[squaresArray.indexOf(e.target)] = playerMarker;
 
-            console.log(currentBoardState);
-
+            //update the unoccupiedSquaresIndexes array to reflect the new marker
             unoccupiedSquaresIndexes = getUnoccupiedSquareIndexes(currentBoardState);
-
-            console.log(unoccupiedSquaresIndexes);
-
-            if (gameEndPatternExists(displayController.playerArray
-            [displayController.playerPointer].getMarker())) {
+            
+            winnerInfo = gameEndPatternExists(playerMarker);
+            //check if the game has ended
+            if (winnerInfo[0]) {
+                declareWinner(winnerInfo[1])
+                changeWinningSquaresColor(winnerInfo.slice(2, 5));
+                return;
+            } else if (winnerInfo[0] === null) {
+                declareWinner(null);
                 return;
             }
 
             changePlayer(displayController.playerPointer);
             displayController.updateTurnText(displayController.playerTurn, displayController.playerArray, displayController.playerPointer);
 
-            console.log(displayController.playerArray[displayController.playerPointer].getMarker())
             aiTurn();
         }
+    }
+
+    const changeWinningSquaresColor = (winnerInfo) => {
+        winnerInfo.forEach((square) => {
+            square.classList.add("win-squares");
+        })
     }
 
     const changePlayer = (playerPointer) => {
@@ -86,226 +80,153 @@ const gameBoard = (() => {
     }
 
     const aiTurn = () => {
-        console.log(displayController.playerArray[displayController.playerPointer].getMarker())
 
-        const bestPlayInfo = minimax(null, currentBoardState, unoccupiedSquaresIndexes, displayController.playerArray[displayController.playerPointer].getMarker());
+        const aiMarker = displayController.playerArray[displayController.playerPointer].getMarker();
+        //get the most optimum next move for the ai
+        const bestPlayInfo = minimax(currentBoardState, aiMarker);
 
-        
-        console.log(bestPlayInfo)
-        console.log(displayController.playerArray[displayController.playerPointer].getMarker())
-        
-        
-        squaresArray[bestPlayInfo.index].textContent = displayController.playerArray[displayController.playerPointer].getMarker();
-        
-        squaresArray[bestPlayInfo.index].dataset.status = "occupied";
-        
-        updateCurrentBoardStatus(squaresArray.indexOf(squaresArray[bestPlayInfo.index]), displayController.playerArray[displayController.playerPointer].getMarker());
-        console.log(currentBoardState);
+        //update the textContent of the clicked square to the ai's marker
+        squaresArray[bestPlayInfo.index].textContent = aiMarker;
+
+        //update the currentBoardState array to reflect the new marker
+        currentBoardState[squaresArray.indexOf(squaresArray[bestPlayInfo.index])] = aiMarker;
 
         unoccupiedSquaresIndexes = getUnoccupiedSquareIndexes(currentBoardState);
-        console.log(unoccupiedSquaresIndexes);
-        
-        changePlayer(displayController.playerPointer);
 
-        displayController.updateTurnText(displayController.playerTurn, displayController.playerArray, displayController.playerPointer);
-        if (gameEndPatternExists(displayController.playerArray
-            [displayController.playerPointer].getMarker())) {
-                return;
+        winnerInfo = gameEndPatternExists(aiMarker);
+        //check if the game has ended
+        if (winnerInfo[0]) {
+            declareWinner(displayController.playerArray[displayController.playerPointer].getMarker())
+            changeWinningSquaresColor(winnerInfo.slice(2, 5));
+            return;
+        }
+        else if (winnerInfo[0] === null) {
+            declareWinner(null);
+            return;
         }
 
         changePlayer(displayController.playerPointer);
-
         displayController.updateTurnText(displayController.playerTurn, displayController.playerArray, displayController.playerPointer);
-
-        // unoccupiedSquares[0]
     }
 
-    const minimax = (savedIndex, currentBoardState, unoccupiedSquaresIndexes, currentMarker) => {
-        console.log(currentMarker)
-        console.log("currentBoardState", currentBoardState)
-        console.log("unoccupiedSquaresIndexes", unoccupiedSquaresIndexes)
+    const minimax = (currentBoardState, currentMarker) => {
 
-        if (gameEndPatternExists("O")) {
-            console.log("result")
+        const unoccupiedSquaresIndexes = getUnoccupiedSquareIndexes(currentBoardState);
+
+        if (gameEndPatternExists("O")[0]) {
             return { score: -1 };
-        } else if (gameEndPatternExists("X")) {
-            console.log("result") 
+        }
+        else if (gameEndPatternExists("X")[0]) {
             return { score: 1 }
-        } else if (drawPatternExists()) {
-            console.log("draw")
+        }
+        else if (unoccupiedSquaresIndexes.length === 0) {
             return { score: 0 };
         }
 
-        const allTestPlayInfos = [];
+        const allPossibleMovesInfo = [];
 
         for (let i = 0; i < unoccupiedSquaresIndexes.length; i++) {
+            const currentMoveInfo = {};
 
-            console.log("i=", i, currentMarker)
-            const currentTestPlayInfo = {};
+            currentMoveInfo.index = currentBoardState[unoccupiedSquaresIndexes[i]];
 
-            currentTestPlayInfo.index = currentBoardState[unoccupiedSquaresIndexes[i]];
+            currentBoardState[unoccupiedSquaresIndexes[i]] = currentMarker;
 
             squaresArray[unoccupiedSquaresIndexes[i]].textContent = currentMarker;
 
-            squaresArray[unoccupiedSquaresIndexes[i]].dataset.status = "occupied";
-
-            savedIndex = unoccupiedSquaresIndexes[i];
-            console.log(savedIndex);
-
-            console.log("marked marked and occupied")
-            updateCurrentBoardStatus(squaresArray.indexOf(squaresArray[unoccupiedSquaresIndexes[i]]), currentMarker);
-            console.log("currentBoardState", currentBoardState);
-
-            unoccupiedSquaresIndexes = getUnoccupiedSquareIndexes(currentBoardState);
-            console.log("unoccupiedSquaresIndexes", unoccupiedSquaresIndexes);
-
             if (currentMarker === "X") {
-                let result = minimax(savedIndex, currentBoardState, unoccupiedSquaresIndexes, "O");
-                currentTestPlayInfo.score = result.score;
-                console.log(currentTestPlayInfo.score);
+                let result = minimax(currentBoardState, "O");
+                currentMoveInfo.score = result.score;
             } else {
-                let result = minimax(savedIndex, currentBoardState, unoccupiedSquaresIndexes, "X");
-                currentTestPlayInfo.score = result.score;
-                console.log(currentTestPlayInfo.score);
+                let result = minimax(currentBoardState, "X");
+                currentMoveInfo.score = result.score;
             }
 
-            console.log(currentMarker, "2nd")
-
-            updateCurrentBoardStatus(savedIndex, savedIndex);
-            console.log("currentBoardState", currentBoardState);
-
-            unoccupiedSquaresIndexes = getUnoccupiedSquareIndexes(currentBoardState);
-            console.log("unoccupiedSquaresIndexes", unoccupiedSquaresIndexes);
-
-            console.log(unoccupiedSquaresIndexes[i]);
-            console.log(currentBoardState);
-            console.log(unoccupiedSquaresIndexes);
+            currentBoardState[unoccupiedSquaresIndexes[i]] = currentMoveInfo.index;
 
             squaresArray[unoccupiedSquaresIndexes[i]].textContent = "";
 
-            squaresArray[unoccupiedSquaresIndexes[i]].dataset.status = "unoccupied";
-
-            allTestPlayInfos.push(currentTestPlayInfo);
-            console.log(allTestPlayInfos)
-            console.log(currentTestPlayInfo)
+            allPossibleMovesInfo.push(currentMoveInfo);
         }
 
         let bestTestPlay = null;
 
-        console.log(allTestPlayInfos)
         if (currentMarker === "X") {
             let bestScore = -Infinity;
-            for (let i = 0; i < allTestPlayInfos.length; i++) {
-                if (allTestPlayInfos[i].score > bestScore) {
-                    bestScore = allTestPlayInfos[i].score;
-                    bestTestPlay = i;
-                }
-            }
-        } else {
-            let bestScore = Infinity;
-            for (let i = 0; i < allTestPlayInfos.length; i++) {
-                if (allTestPlayInfos[i].score < bestScore) {
-                    bestScore = allTestPlayInfos[i].score;
+            for (let i = 0; i < allPossibleMovesInfo.length; i++) {
+                if (allPossibleMovesInfo[i].score > bestScore) {
+                    bestScore = allPossibleMovesInfo[i].score;
                     bestTestPlay = i;
                 }
             }
         }
-        console.log(allTestPlayInfos[bestTestPlay])
-        return allTestPlayInfos[bestTestPlay];
+        else {
+            let bestScore = Infinity;
+            for (let i = 0; i < allPossibleMovesInfo.length; i++) {
+                if (allPossibleMovesInfo[i].score < bestScore) {
+                    bestScore = allPossibleMovesInfo[i].score;
+                    bestTestPlay = i;
+                }
+            }
+        }
+        return allPossibleMovesInfo[bestTestPlay];
     }
 
     const gameEndPatternExists = (playerMarker) => {
-        // let squares = getSquares();
 
-        if (horizontalWinningPattern(playerMarker, squaresArray, displayController.playerArray)) return true;
+        if (horizontalWinningPattern(playerMarker, squaresArray)[0]) return [true, ...horizontalWinningPattern(playerMarker, squaresArray).slice(1, 5)];
 
-        if (vericalWinningPatternExists(playerMarker, squaresArray, displayController.playerArray)) return true;
+        if (vericalWinningPatternExists(playerMarker, squaresArray)[0]) return [true, ...vericalWinningPatternExists(playerMarker, squaresArray).slice(1, 5)];
 
-        if (diagonalWinningPatternExists(playerMarker, squaresArray, displayController.playerArray)) return true;
+        if (diagonalWinningPatternExists(playerMarker, squaresArray)[0]) return [true, ...diagonalWinningPatternExists(playerMarker, squaresArray).slice(1, 5)];
 
         if (drawPatternExists()) {
-            declareWinner(null);
-            return true;
+            return [null];
         }
-        return false;
+        return [false];
     }
 
     //checks the horizontal winning conditon for a player
-    const horizontalWinningPattern = (playerMarker, squaresArray, playerArray) => {
+    const horizontalWinningPattern = (playerMarker, squaresArray) => {
         let i = 0;
         while (i < 9) {
             if (squaresArray[i].textContent === squaresArray[i + 1].textContent && squaresArray[i + 1].textContent === squaresArray[i + 2].textContent && squaresArray[i].textContent !== "" && squaresArray[i].textContent === playerMarker) {
-                for (let player of playerArray) {
-                    //checks which marker value wins and matches it to the corresponding player
-                    if (player.getMarker() === playerMarker) {
-                        declareWinner(player);
-
-                        // squaresArray[i].classList.add("win-squares");
-                        // squaresArray[i + 1].classList.add("win-squares");
-                        // squaresArray[i + 2].classList.add("win-squares");
-                    }
-                }
-                return true;
+                return [true, playerMarker, squaresArray[i], squaresArray[i + 1], squaresArray[i + 2]]
             }
             i = i + 3;
         }
+        return [false];
     }
 
     //checks the vertical winning conditon for a player
-    const vericalWinningPatternExists = (playerMarker, squaresArray, playerArray) => {
+    const vericalWinningPatternExists = (playerMarker, squaresArray) => {
         let j = 0;
         while (j < 3) {
             if (squaresArray[j].textContent === squaresArray[j + 3].textContent && squaresArray[j + 3].textContent === squaresArray[j + 6].textContent && squaresArray[j].textContent !== "" && squaresArray[j].textContent === playerMarker) {
-                for (let player of playerArray) {
-                    if (player.getMarker() === playerMarker) {
-                        declareWinner(player);
-                        // squaresArray[j].classList.add("win-squares");
-                        // squaresArray[j + 3].classList.add("win-squares");
-                        // squaresArray[j + 6].classList.add("win-squares");
-                    }
-                };
-                return true;
+                return [true, playerMarker, squaresArray[j], squaresArray[j + 3], squaresArray[j + 6]]
             }
             j++;
         }
+        return [false];
     }
 
     //checks the diagonal winning conditon for a player
-    const diagonalWinningPatternExists = (playerMarker, squaresArray, playerArray) => {
+    const diagonalWinningPatternExists = (playerMarker, squaresArray) => {
         let k = 0;
         while (k < 3) {
             if (k === 0) {
                 if (squaresArray[k].textContent === squaresArray[k + 4].textContent && squaresArray[k + 4].textContent === squaresArray[k + 8].textContent && squaresArray[k].textContent !== "" && squaresArray[k].textContent === playerMarker) {
-                    for (let player of playerArray) {
-                        if (player.getMarker() === playerMarker) {
-
-                            // declareWinner(player);
-                            // squaresArray[k].classList.add("win-squares");
-                            // squaresArray[k + 4].classList.add("win-squares");
-                            // squaresArray[k + 8].classList.add("win-squares");
-
-                        }
-                    }
-                    return true;
+                    return [true, playerMarker, squaresArray[k], squaresArray[k + 4], squaresArray[k + 8]]
                 }
             }
-
             else {
-                if (squaresArray[k].textContent === squaresArray[k + 2].textContent && squaresArray[k + 2].textContent === squaresArray[k + 4].textContent && squaresArray[k].textContent !== "") {
-                    for (let player of displayController.playerArray) {
-                        if (player.getMarker() === squaresArray[k].textContent) {
-
-                            // declareWinner(player);
-                            // squaresArray[k].classList.add("win-squares");
-                            // squaresArray[k + 2].classList.add("win-squares");
-                            // squaresArray[k + 4].classList.add("win-squares");
-                        }
-                    }
-                    return true;
+                if (squaresArray[k].textContent === squaresArray[k + 2].textContent && squaresArray[k + 2].textContent === squaresArray[k + 4].textContent && squaresArray[k].textContent !== "" && squaresArray[k].textContent === playerMarker) {
+                    return [true, playerMarker, squaresArray[k], squaresArray[k + 2], squaresArray[k + 4]];
                 }
             }
             k += 2;
         }
+        return [false];
     }
 
     const drawPatternExists = () => {
@@ -314,19 +235,16 @@ const gameBoard = (() => {
         }
     }
 
-    const declareWinner = (player) => {
-        if (player) {
-            displayController.updateWinnerName(player.getName());
+    const declareWinner = (playerMarker) => {
+        if (playerMarker === null) displayController.updateWinnerName("Game Draw!");
+        else {
+            for (let player of displayController.playerArray) {
+                if (player.getMarker() === playerMarker) {
+                    displayController.updateWinnerName(player.getName());
+                }
+            }
         }
-        else displayController.updateWinnerName("Game Draw!");
-        displayController.playerPointer = 0;
-        for (let square of squaresArray) {
-            square.removeEventListener("click", onClick);
-        }
-        numberOfSquaresClicked = 0;
-        unoccupiedSquares = [];
     }
-
     return { newGame };
 })();
 
@@ -355,3 +273,4 @@ const displayController = (() => {
 })();
 
 gameBoard.newGame();
+// gameBoard.aiTurn();
